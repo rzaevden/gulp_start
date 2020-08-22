407b356abab89137f9f368fa55a2a9703b3ed160
GlistValera <den_rzaev@mail.ru>
2020-08-22 16:37:14 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const { src, dest, series, parallel, watch } = require('gulp');
+
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const groupMediaQueries = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-cleancss');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const replace = require('gulp-replace');
+const del = require('del');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync').create();
+
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const imagemin = require('gulp-imagemin');
+
+const paths =  {
+  src: './src/',              // paths.src
+  build: './build/'           // paths.build
+};
+
+function styles() {
+  return src(paths.src + 'scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass()) // { outputStyle: 'compressed' }
+    .pipe(groupMediaQueries())
+    .pipe(postcss([
+      autoprefixer({overrideBrowserslist: ['last 2 version']}),
+    ]))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write('/'))
+    .pipe(dest(paths.build + 'css/'));
+}
+
+function svgSprite() {
+  return src(paths.src + 'svg/*.svg')
+    .pipe(svgmin(function (file) {
+      return {
+        plugins: [{
+          cleanupIDs: {
+            minify: true
+          }
+        }]
+      }
+    }))
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename('sprite-svg.svg'))
+    .pipe(dest(paths.build + 'img/'));
+}
+
+function scripts() {
+  return src(paths.src + 'js/*.js')
+    .pipe(plumber())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat('script.min.js'))
+    .pipe(dest(paths.build + 'js/'))
+}
+
+function scriptsVendors() {
+  return src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/slick-carousel/slick/slick.min.js',
+      'node_modules/svg4everybody/dist/svg4everybody.min.js'
+    ])
+    .pipe(concat('vendors.min.js'))
+    .pipe(dest(paths.build + 'js/'))
+}
+
+function htmls() {
+  return src(paths.src + '*.html')
+    .pipe(plumber())
+    .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
+    .pipe(dest(paths.build));
+}
+
+function images() {
+  return src(paths.src + 'img/*.{jpg,jpeg,png,gif,svg}')
+    //.pipe(imagemin()) // если картинок будет много, то и времени будет уходить много
+    .pipe(dest(paths.build + 'img/'));
+}
+
+function clean() {
+  return del('build/')
+}
+
+function watcher() {
+  watch(paths.src + 'scss/*.scss', styles);
+  watch(paths.src + 'js/*.js', scripts);
+  watch(paths.src + '*.html', htmls);
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: paths.build
+    }
+  });
+  browserSync.watch(paths.build + '**/*.*', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.scriptsVendors = scriptsVendors;
+exports.htmls = htmls;
+exports.images = images;
+exports.svgSprite = svgSprite;
+exports.clean = clean;
+exports.watcher = watcher;
+
+exports.build = series(
+  clean,
+  parallel(styles, svgSprite, scripts, scriptsVendors, htmls, images)
+);
+
+exports.default = series(
+  clean,
+  parallel(styles, svgSprite, scripts, scriptsVendors, htmls, images),
+  parallel(watcher, serve),
+);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Демо автоматизации с gulp 4
+
+Установка: `npm i`.
+
+Запуск сервера разработки: `npm start`
+
+Остановка сервера: `Ctrl + C`
+
+
+
+## К прочтению / ознакомлению
+
+- [Как работать с CSS-препроцессорами и БЭМ](http://nicothin.github.io/idiomatic-pre-CSS/)
+- [Шпаргалка по bash](https://github.com/nicothin/web-development/tree/master/bash)
+- [Шпаргалка по консольным командам Git](https://github.com/nicothin/web-development/tree/master/git)
+- [Шпаргалка ниндзя Sublime Text 3](http://nicothin.github.io/sublime-text/sublime-text-3-hotkeys.html)
